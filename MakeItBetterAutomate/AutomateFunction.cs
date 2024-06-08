d29178636e5c14459149e68350de19b03049403f
David Morgan <haitheredavid@gmail.com>
2024-06-08 19:36:14 -0400

    seeing if dalle ref breaks shit

--- MakeItBetterAutomate/AutomateFunction.cs
+++ MakeItBetterAutomate/AutomateFunction.cs
@@ -1,11 +1,15 @@
 using Objects;
+using OpenAI.Managers;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI;
 using Speckle.Automate.Sdk;
 using Speckle.Core.Models.Extensions;
+using OpenAI.Interfaces;
 
 public static class AutomateFunction
 {
     public static async Task Run(
-      AutomationContext automationContext,
+      AutomationContext automationContext,  
       FunctionInputs functionInputs
     )
     {
@@ -16,11 +20,33 @@ public static class AutomateFunction
         Console.WriteLine("Receiving version");
         var commitObject = await automationContext.ReceiveVersion();
 
+        try
+        {
+            var openAiService = new OpenAIService(new OpenAiOptions()
+            {
+                ApiKey = functionInputs.ExternalServiceKey
+
+            });
+
+            var imageCreateRequest = new ImageEditCreateRequest();
+
+            var image = await openAiService.CreateImage(functionInputs.Vibe.ToString());
+
+            Console.WriteLine("is image successful?", image.Successful == true);
+
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
 
         var url= automationContext.SpeckleClient.ServerUrl;
 
         Console.WriteLine("Current Server: " + url);
 
-        automationContext.MarkRunSuccess("Is now better");
+        automationContext.MarkRunSuccess("\nIs now better\n");
+        
+        //automationContext.StoreFileResult(url);
     }
 }
